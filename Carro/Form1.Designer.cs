76287ee777e3442386af996cf0171f93bcd5bd1b
Fabian <fabianes1996@gmail.com>
2018-08-23 18:32:35 -0500

    Primera Versión

---
+++ Carro/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Carro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblpuntos = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.carro1 = new System.Windows.Forms.PictureBox();
+            this.carro2 = new System.Windows.Forms.PictureBox();
+            this.lblPuntos2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.carro1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carro2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblpuntos
+            // 
+            this.lblpuntos.AutoSize = true;
+            this.lblpuntos.Location = new System.Drawing.Point(31, 30);
+            this.lblpuntos.Name = "lblpuntos";
+            this.lblpuntos.Size = new System.Drawing.Size(35, 13);
+            this.lblpuntos.TabIndex = 1;
+            this.lblpuntos.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // carro1
+            // 
+            this.carro1.BackColor = System.Drawing.Color.Transparent;
+            this.carro1.Image = global::Carro.Properties.Resources.Carro1;
+            this.carro1.Location = new System.Drawing.Point(12, 293);
+            this.carro1.Name = "carro1";
+            this.carro1.Size = new System.Drawing.Size(65, 115);
+            this.carro1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.carro1.TabIndex = 0;
+            this.carro1.TabStop = false;
+            // 
+            // carro2
+            // 
+            this.carro2.BackColor = System.Drawing.Color.Transparent;
+            this.carro2.BackgroundImage = global::Carro.Properties.Resources.Carro_2_T;
+            this.carro2.Image = global::Carro.Properties.Resources.Carro_2_T;
+            this.carro2.Location = new System.Drawing.Point(168, 293);
+            this.carro2.Name = "carro2";
+            this.carro2.Size = new System.Drawing.Size(65, 115);
+            this.carro2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.carro2.TabIndex = 2;
+            this.carro2.TabStop = false;
+            // 
+            // lblPuntos2
+            // 
+            this.lblPuntos2.AutoSize = true;
+            this.lblPuntos2.Location = new System.Drawing.Point(261, 30);
+            this.lblPuntos2.Name = "lblPuntos2";
+            this.lblPuntos2.Size = new System.Drawing.Size(35, 13);
+            this.lblPuntos2.TabIndex = 3;
+            this.lblPuntos2.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Carro.Properties.Resources.carre2;
+            this.ClientSize = new System.Drawing.Size(308, 420);
+            this.Controls.Add(this.lblPuntos2);
+            this.Controls.Add(this.carro2);
+            this.Controls.Add(this.lblpuntos);
+            this.Controls.Add(this.carro1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.carro1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carro2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox carro1;
+        private System.Windows.Forms.Label lblpuntos;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox carro2;
+        private System.Windows.Forms.Label lblPuntos2;
+    }
+}
+
---
+++ Carro/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Carro
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> ListaObstaculos = new List<PictureBox>();
+        List<PictureBox> ListaObstaculos2 = new List<PictureBox>();
+        Random RnTipoObstaculo = new Random();
+        int velocidad1 = 5;
+        int velocidad2 = 5;       
+        int totalPuntos1;
+        int totalPuntos2;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            lblpuntos.Text = "0";
+            lblPuntos2.Text = "0";
+            CrearObstaculo(ListaObstaculos, this, 10, 80);
+            CrearObstaculo(ListaObstaculos2, this, 180,250);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox ImagenCarro in ListaObstaculos2)
+            {
+                int MovimientoY;
+                MovimientoY = ImagenCarro.Location.Y;
+                MovimientoY = MovimientoY + velocidad2;
+                ImagenCarro.Location = new Point(ImagenCarro.Location.X, MovimientoY);
+            }
+            if (ListaObstaculos2.Count > 0)
+            {
+                if (ListaObstaculos2[(ListaObstaculos2.Count) - 1].Location.Y > 250)
+                {
+                    CrearObstaculo(ListaObstaculos2, this, 180, 250);
+                }
+            }
+            if (ListaObstaculos2.Count > 0)
+            {
+                for (int i = 0; i < ListaObstaculos2.Count; i++)
+                {
+                    if (ListaObstaculos2[i].Location.Y > 400)
+                    {
+                        if (ListaObstaculos2[i].Tag.ToString() == "1_1")
+                        {
+                            velocidad2=5;
+                        }
+                        Controls.Remove(ListaObstaculos2[i]);
+                        ListaObstaculos.Remove(ListaObstaculos2[i]);
+                    }
+                    if (ListaObstaculos2[i].Bounds.IntersectsWith(carro2.Bounds))
+                    {
+                        if (ListaObstaculos2[i].Tag.ToString() == "1_2")
+                        {
+                            Controls.Remove(ListaObstaculos2[i]);
+                            
+                            totalPuntos2 = int.Parse(lblPuntos2.Text) + 1;
+                           
+                            if (totalPuntos2 % 2 == 0)
+                            {
+                                velocidad2++;
+                            }
+                            
+                            lblPuntos2.Text = totalPuntos2.ToString();
+                            ListaObstaculos2.Remove(ListaObstaculos2[i]);
+                        }
+                        else
+                        {
+                            Controls.Remove(ListaObstaculos2[i]);
+                            ListaObstaculos2.Remove(ListaObstaculos2[i]);
+                            velocidad2 = 5;
+                            int totalPuntos2 = int.Parse(lblPuntos2.Text) - 1;
+                            lblPuntos2.Text = totalPuntos2.ToString();
+                        }
+                    }
+                }
+            }
+        }
+        public void CrearObstaculo(List<PictureBox> ListaElementos, Form panelJuegoUno, int distanciaUno, int distanciaDos)
+        {
+            int NumeroCarro = 1;
+            int TipoObstaculo = RnTipoObstaculo.Next(1, 3);
+            int UbicacionObstaculo = RnTipoObstaculo.Next(1, 3);
+            int DistanciaUbicacionObstaculo = (UbicacionObstaculo == 1) ? distanciaUno : distanciaDos;
+            PictureBox pb = new PictureBox();
+            pb.Location = new Point(DistanciaUbicacionObstaculo, 0);
+            pb.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("Obstaculo" + NumeroCarro + TipoObstaculo);
+            pb.BackColor = Color.Transparent;
+            pb.Tag = NumeroCarro + "_" + TipoObstaculo;
+            pb.SizeMode = PictureBoxSizeMode.AutoSize;
+            ListaElementos.Add(pb);
+            panelJuegoUno.Controls.Add(pb);
+
+        }
+        
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int CambioCarro = (carro1.Location.X == 80) ? 10 : 80;
+            carro1.Location = new Point(CambioCarro, carro1.Location.Y);
+
+            int CambioCarro2 = (carro2.Location.X == 230) ? 165 : 230;
+            carro2.Location = new Point(CambioCarro2, carro2.Location.Y);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox ImagenCarro in ListaObstaculos)
+            {
+                int MovimientoY;
+                MovimientoY = ImagenCarro.Location.Y;
+                MovimientoY = MovimientoY + velocidad1;
+                ImagenCarro.Location = new Point(ImagenCarro.Location.X, MovimientoY);
+            }
+            if (ListaObstaculos.Count>0 )
+            {
+                if (ListaObstaculos[(ListaObstaculos.Count)-1].Location.Y>250)
+                {
+                    CrearObstaculo(ListaObstaculos, this, 10, 80);
+                    
+                }
+            }
+            if (ListaObstaculos.Count>0)
+            {
+                for (int i = 0; i < ListaObstaculos.Count; i++)
+                {
+                    if (ListaObstaculos[i].Location.Y>400)
+                    {
+                        if (ListaObstaculos[i].Tag.ToString()=="1_1")
+                        {
+                            velocidad1 = 5;                            
+                        }
+                        Controls.Remove(ListaObstaculos[i]);
+                        ListaObstaculos.Remove(ListaObstaculos[i]);
+                    }
+                    if (ListaObstaculos[i].Bounds.IntersectsWith(carro1.Bounds))
+                    {
+                        if (ListaObstaculos[i].Tag.ToString()=="1_2")
+                        {
+                            Controls.Remove(ListaObstaculos[i]);
+                            totalPuntos1= int.Parse(lblpuntos.Text)+1;                           
+
+                            if (totalPuntos1 %2==0)
+                            {
+                                velocidad1++;
+                            }
+                            
+                            lblpuntos.Text = totalPuntos1.ToString();
+                            ListaObstaculos.Remove(ListaObstaculos[i]);
+                        }
+                        else
+                        {
+                            Controls.Remove(ListaObstaculos[i]);
+                            ListaObstaculos.Remove(ListaObstaculos[i]);
+                            velocidad1 = 5;
+                            totalPuntos1 = int.Parse(lblpuntos.Text) - 1;
+                            lblpuntos.Text = totalPuntos1.ToString();
+                        }
+                    }              
+                }
+            }
+            
+        }      
+
+    }
+}
